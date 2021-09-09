cfe06e9d82237ad9eef1de5d1b0d4a865243770b
Thomas E <einstein.thomas2@gmail.com>
2021-09-08 17:02:13 -0700

    added documentation

--- middleware.go
+++ middleware.go
@@ -4,6 +4,7 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// Middleware provides echo middleware for `echo.Echo.Use()`
 func Middleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		return next(NewCtx(c, nil))
